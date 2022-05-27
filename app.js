fea2c4b83f9b0ad2ad11fc64c170916b9abcae44
brenobccc <brenobritobc@gmail.com>
2022-05-27 17:08:55 -0300

    implementacao_crud

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-//incluindo biblioteca
 const http = require('http');
 const url = require('url');
-const queryString = require('query-string');
-
-
+const queryString = require('query-string')
 //Define o IP e a Porta que a aplicação vai rodar
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -11,28 +8,21 @@ const port = 3000;
 //Implementação de regra de negócio
 const server = http.createServer((req, res) => {
 
-    //Pega a pergunta na URL
-    const params = queryString.parse(
-        url.parse(req.url, true).search
-    );
-    // console.log("Parametros:",params);
-    let resposta;
-    //Verificar a pergunta e escolher uma respota
-    if(params.pergunta == 'melhor-filme'){
-        resposta = 'one piece';
-    }else if(params.pergunta == 'melhor-tecnologia-backend'){
-        resposta = 'nodejs';
-    }else{
-        resposta = 'nao sei';
-    }
+    //Criar usuario
+        //receber informações do usuario
+        const usuario = queryString.parse(url.parse(req.url, true).search);
+        console.log(usuario);
+        //Salvar informacoes
 
-    //Retornar a respot escolhida
+
+    //Atualizar usuario
+    //Pesquisar(get)
+    //Remover usuario
+
 
     res.statusCode = 200;//Códig
     res.setHeader('Content-Type', 'text/plain');
-    // res.end('Hello World');
-    // res.end(params.pergunta);
-    res.end(resposta);
+    res.end("Hello world!!");
 });
 
 // Onde o código começa a executar
